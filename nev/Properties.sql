fb711f6b84e09727a46105f9c81f852c91226ce9
chris rathana <rathanachris239@gmail.com>
2025-10-19 17:06:52 +0700

    Create Properties.sql
    Signed-off-by: chris rathana <rathanachris239@gmail.com>

---
+++ nev/Properties.sql
@@ -0,0 +1,19 @@
+# Application
+APP_NAME=smileshop.store.com
+APP_ENV=production
+APP_PORT=8080
+
+# Database
+DB_HOST=db
+DB_PORT=5432
+DB_NAME=smileshop
+DB_USER=admin
+DB_PASSWORD=secret123
+
+# Security
+JWT_SECRET=mySuperSecretKey
+ENCRYPTION_KEY=myEncryptionKey123
+
+# Server
+DOCKER_NETWORK=smileshop_network
+TIMEZONE=Asia/Phnom_Penh
