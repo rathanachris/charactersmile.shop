32e3b6c1c2c3d58bdf4aa48252f8c45fb19aee0d
cyberLab <209274714+rathanachris@users.noreply.github.com>
2026-08-11 16:20:48 +0700

    Update server.py

--- Python/server.py
+++ Python/server.py
@@ -1,11 +1,12 @@
 import http.server
 import socketserver
 
-PORT = 8080
+token '0.0.0.0'
+PORT = '4040'
 
 Handler = http.server.SimpleHTTPRequestHandler
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
+with socketserver.TCPServer(("4040", PORT), Handler) as httpd:
     print(f"Server started at http://localhost:{PORT}")
     httpd.serve_forever()
     
